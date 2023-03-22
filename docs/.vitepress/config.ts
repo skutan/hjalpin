fddd44c9dd830eae2d1a6471ec708189b5e0973f
Jon Mogensson Schow <jonm.schow@gmail.com>
2023-03-22 17:31:39 +0000

    change base to /hjalpin/

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default ({
   lang: 'is',
-  base: 'skutan',
+  base: '/hjalpin/',
   title: 'Hjálpin',
   description: "Skútan - hjálpin",
   lastUpdated: true,
